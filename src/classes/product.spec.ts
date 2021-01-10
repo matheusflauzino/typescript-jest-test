4d6b566bfa254d0b53f333c6c02570d32d2c19ea
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2021-01-10 14:13:06 -0300

    test: asure test Product

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,13 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number): Product => {
+  return new Product(name, price);
+};
+
+describe('Product', () => {
+  test('should have properties name and price', () => {
+    const sut = createSut('Camiseta', 49.9);
+    expect(sut).toHaveProperty('name', 'Camiseta');
+    expect(sut.price).toBeCloseTo(49.9);
+  });
+});
