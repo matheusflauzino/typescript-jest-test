514380772bf2d842f54c339e1d76b51f1bb930e1
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2021-01-10 13:18:07 -0300

    test: asure test Persistency

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,7 +1,26 @@
 import { Persistency } from './persistency';
 
 describe('Persistency', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('should return undefined', () => {
-    expect(1).toBe(1);
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  test('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  test('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
   });
 });
